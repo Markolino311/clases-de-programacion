34206c3491c3c8fe782ded8945ab1e0d420b439a
Markolino311 <69398537+Markolino311@users.noreply.github.com>
2024-07-08 16:29:28 +0000

    Se añadió un mensaje de bienvenida a Eduardo, después de imprimir la suma de ambos números.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numero1=int(input("Ingrese un número para hacer una suma: "))
-numero2=int(input("Ingresa el segundo número: "))
+numero1 = int(input("Ingrese un número para hacer una suma: "))
+numero2 = int(input("Ingresa el segundo número: "))
 
-print(numero1+numero2)
+print(numero1 + numero2)
+print("Hola Eduardo. Bienvenido a la clase de Python")
